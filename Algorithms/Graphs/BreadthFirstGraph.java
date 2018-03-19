3802d736fc823d1ae825ebf74d2f8f8adf2f6a24
kosaraju.a@husky.neu.edu <kosaraju.a@husky.neu.edu>
2018-03-18 23:16:40 -0400

    Graphs folder Added with basic algorithm programs

---
+++ Algorithms/Graphs/BreadthFirstGraph.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basics;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+
+/**
+ *
+ * @author sowjanya
+ */
+public class BreadthFirstGraph {
+
+    private int V;   // No. of vertices
+    private LinkedList<Integer> adj[]; //Adjacency Lists
+
+    // Constructor
+    BreadthFirstGraph(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList();
+        }
+    }
+
+    class BFS {
+
+    }
+
+    // Function to add an edge into the graph
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    // prints BFS traversal from a given source s
+    void BFS(int s) {
+        // Mark all the vertices as not visited(By default the values of the array elements
+        //will be set to false when the boolean type array is created)
+        boolean visited[] = new boolean[V];
+
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+
+        // Mark the current node as visited and enqueue it
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    // Driver method to
+    public static void main(String args[]) {
+        BreadthFirstGraph g = new BreadthFirstGraph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Breadth First Traversal "
+                + "(starting from vertex 2)");
+
+        g.BFS(2);
+    }
+}
